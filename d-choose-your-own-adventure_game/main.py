d40ad9b5bbc575ac7d4bfc4ec499f2de397fd245
Abhinav08bhatt <abhinav08bhatt@gmail.com>
2025-11-27 11:38:23 +0530

    working on project 4

---
+++ d-choose-your-own-adventure_game/main.py
@@ -0,0 +1,32 @@
+from utilities import clear , style , press_key
+
+def valid_input():
+
+
+    pass
+
+def inventory():
+
+    pass
+
+
+def health():
+
+    pass
+
+
+def ui():
+
+    pass
+
+
+def game():
+
+    pass
+
+if __name__ == "__main__":
+
+    clear()
+    
+
+    pass
---
+++ d-choose-your-own-adventure_game/utilities.py
@@ -0,0 +1,21 @@
+import os
+from time import sleep
+
+def clear():
+    '''
+    to clear the terminal for a better console UI
+    '''
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def style(s,n=0.04):
+    '''
+    nothing, just attention to details
+    :param s: string input
+    :param n: speed of text (default 0.04)
+    '''
+    for i in range (0,len(s)):
+        print(s[i],end='')
+        sleep(n)
+
+def press_key():
+    input("\n\nPress ENTER to continue...")
