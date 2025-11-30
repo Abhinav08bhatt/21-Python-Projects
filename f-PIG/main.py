d15db083b40e2339f8a124ba8529e056082111d2
Abhinav08bhatt <abhinav08bhatt@gmail.com>
2025-11-30 09:10:38 +0530

    completed project 6

--- f-PIG/main.py
+++ f-PIG/main.py
@@ -6,7 +6,7 @@ def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def press_key():
-    style("\n\nPress ENTER to ROLL THE DIE...")
+    style("\nPress ENTER to ROLL THE DIE...")
     input()
 
 def style(s,n=0.03):
@@ -53,7 +53,10 @@ def outcome_others(points):
     style("\n\nLucky roll!\n")
     style(f"You earned {points} points from this outcome.\n")
 
-def player_input(score):
+def player_input(player_num,score,end_score):
+    print("\n==================================================")
+    style(f"                    PLAYER {player_num}\n")
+    print("==================================================")
     bet = int_input("Enter the number of times u want to roll the die - ")
     chance_score = 0
     for chance in range (1,bet+1):
@@ -70,20 +73,43 @@ def player_input(score):
     print("\n")
     style(f"You earned {chance_score} points in this round!!!\n")
     score += chance_score
+    if score >= end_score:
+        return score
     style(f"Final score is now : {score}\n")
+    sleep(2)
+    return score
 
+def ui(player_score,end_score):
+    
+    print("==================================================")
+    print(f"Score to reach : {end_score}")
+    print("==================================================")
+    for i in range (0,len(player_score)):
+        print(f"{player_score[i][0]} : {player_score[i][1]}")
+    print("==================================================")
 
-
-def score_calculator(score):
-    # 2,3,4,5,6 : adds to score
-    # 1 : score = 0
-    pass
-
-def ui():
-    pass
 
 def game():
-    pass
+    
+    number_of_players,end_score = game_outline()
+    players_score=[
+    ]
+    for i in range (1,number_of_players+1):
+        players_score.append([f"Player {i}",0])
+
+    while True:
+        for player in range (0,number_of_players):
+            clear()
+            ui(players_score,end_score)
+            players_score[player][1] = player_input(player+1,players_score[player][1],end_score)
+            if players_score[player][1] >= end_score:
+                clear()
+                ui(players_score,end_score)
+                style(f"Congratulations {player+1} WON!!!\n")
+                return
+            clear()
+        
 
 if __name__ == "__main__":
-    player_input(0)
+    clear()
+    game()
